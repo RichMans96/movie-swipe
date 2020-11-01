0e2b5e3acb650be2278590d6e28c3cb22af1cfbd
RichMans96 <richard.mansworth@hotmail.co.uk>
2020-11-01 22:00:48 +0000

    fixed bug i was having, now i just need to focus on making the app look nicer!

--- src/Components/Main/Movie/Movie.js
+++ src/Components/Main/Movie/Movie.js
@@ -1,47 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import MovieCard from './MovieCard/MovieCard';
 import { MovieContainer, NextMovie } from './MovieElements';
 
-const API_KEY = 'a035c9128c767a8b70c9413632d63cd0';
-
 const Movie = () => {
+ const [rank, setRank] = useState({ pageNo: 1, movieNo: 1 });
  const [movie, setMovie] = useState('');
  const [isLoading, setIsLoading] = useState(true);
- const [pageNo, setPageNo] = useState(1);
- const [movieNo, setMovieNo] = useState(1);
- const [url, setUrl] = useState('https://image.tmdb.org/t/p/');
+ const [imageUrl, setImageUrl] = useState('https://image.tmdb.org/t/p/');
 
  function handleClick(e) {
   e.preventDefault();
-  setMovieNo(Math.floor(Math.random() * 20));
-  setPageNo(Math.floor(Math.random() * 500 + 1));
+  setRank({
+   pageNo: Math.floor(Math.random() * 500 + 1),
+   movieNo: Math.floor(Math.random() * 20),
+  });
+ }
+
+ async function fetchData(page) {
+  setIsLoading(true);
+  const result = await axios(
+   `https://api.themoviedb.org/3/discover/movie?api_key=a035c9128c767a8b70c9413632d63cd0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&vote_average.gte=1.0`
+  );
+  setMovie(result.data.results);
+  setIsLoading(false);
  }
 
  useEffect(() => {
-  const fetchData = async () => {
-   const result = await axios(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNo}&vote_average.gte=1.0`
-   );
-   setMovie(result.data.results);
-   setIsLoading(false);
-  };
-  fetchData();
- }, []);
-
- console.log(movie);
- console.log(pageNo);
- console.log(movieNo);
+  fetchData(rank.pageNo);
+ }, [rank]);
 
  return (
   <MovieContainer>
    <MovieCard
     movie={movie}
     isLoading={isLoading}
-    url={url}
+    url={imageUrl}
     size={'w342'}
-    movieNum={movieNo}
+    movieNum={rank.movieNo}
    />
    <NextMovie onClick={handleClick}>Next!</NextMovie>
   </MovieContainer>
